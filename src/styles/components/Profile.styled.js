8c50d93a0f015e8ebd9257ba2e4762eff7cedf8f
Hyuneun <hyuneun@skuniv.ac.kr>
2024-05-04 03:25:54 +0900

    ABOUT Page

--- src/styles/components/Profile.styled.js
+++ src/styles/components/Profile.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ProfileLayout = styled.div`
   display: flex;
-  width: 30%;
+  width: 40%;
   height: 137px;
   flex-direction: column;
   align-items: center;
@@ -11,15 +11,19 @@ export const ProfileLayout = styled.div`
 `;
 
 export const ProfileItemBox = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 111px;
+  height: auto;
   display: flex;
   flex-direction: column;
-  background-color: #ffffff;
+  background-image: url('/aboutBox.png');
+  background-size: cover; /* 이미지를 박스에 꽉 채우도록 설정 */
+  background-repeat: no-repeat; /* 이미지 반복 방지 */
   border-radius: 5px;
   justify-content: center;
   align-items: center;
+  padding: 9px;
   margin: 10%;
+  gap: 3px;
 `;
 
 export const ProfileLogoImg = styled.img`
@@ -40,7 +44,9 @@ export const ProfileMajorParagraph = styled.p`
   width: 100%;
   font-family: 'Pretendard Variable', arial;
   font-weight: 300;
+  white-space: nowrap;
   text-align: center;
+
 `;
 
 export const ProfileRoleParagraph = styled.p`
@@ -48,5 +54,6 @@ export const ProfileRoleParagraph = styled.p`
   width: 100%;
   font-family: 'Pretendard Variable', arial;
   font-weight: 300;
-  text-align: center;
+  text-align: left;
+  white-space: nowrap;
 `;
