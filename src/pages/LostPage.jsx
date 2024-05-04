1617fbf08a31920a30cf5f973b5ac3f7453cb8b6
Hyojoong,Kim <115703110+hn7093@users.noreply.github.com>
2024-05-04 18:34:20 +0900

    repeat/api 테스트

--- src/pages/LostPage.jsx
+++ src/pages/LostPage.jsx
@@ -7,8 +7,25 @@ import { LostData } from '../utils/LostData';
 import { Link } from 'react-router-dom';
 import InputButton from '../components/InputButton';
 import i from '../assets/images/menubar/menubarImage.svg';
-export default function ItemsLost() {
+import { useState,useEffect } from 'react';
 
+export default function ItemsLost() {
+  console.log("hi");
+  const [itemData,setData] = useState([]);
+  useEffect(() => {
+    fetch('http://dev.skufestival2024.site/api/notice/posts?lastId=0&size=8')
+  .then(response => response.json())
+  .then(data => {
+    console.log("hi2");
+    // 서버로부터 받은 데이터를 처리합니다.
+    console.log(data);
+    //setData(data);
+  })
+  .catch(error => {
+    // 오류가 발생한 경우 처리합니다.
+    console.error('Error:', error);
+  });},[]);
+  
   return (
     <>
       <MenuBar></MenuBar>
