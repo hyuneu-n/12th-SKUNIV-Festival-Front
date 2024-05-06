2fa84bfa421bd3a86af930b40b3b617e11f7ce9e
Kim So Yeon <qws1566@naver.com>
2024-05-07 01:14:34 +0900

    :bug: 사이즈 재재수정

--- src/components/Specialguest2.jsx
+++ src/components/Specialguest2.jsx
@@ -18,7 +18,7 @@ const SpecaiContainer = styled.div`
 const ItemContainer = styled.div`
   position: relative;
   width: 136px;
-  height: 20vh;
+  height: 22vh;
   background-color: white;
   border-radius: 20px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
