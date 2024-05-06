3a5352ea293c15bc2120e1a46ae2f51c30ddfc4e
Hyuneun <hyuneun@skuniv.ac.kr>
2024-05-06 21:58:37 +0900

    홈화면 화질 개선

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,14 +13,14 @@ import MiniTimeTable from '../components/MiniTimeTable';
 //svg
 import map from '../assets/images/homePage/boothmap.png';
 import pinkMuhly from '../assets/images/homePage/Pinkmuhly.png';
-import test3 from '../assets/images/homePage/test3.png';
-import cd from '../assets/images/homePage/cd1.png';
+import test3 from '../assets/images/homePage/main3.png';
+import cd from '../assets/images/homePage/cdreal.png';
 import playlist from '../assets/images/homePage/playlist.png';
 import AllLogo from '../assets/images/homePage/allLGO.svg';
 import playbar from '../assets/images/homePage/playbar.svg';
 import bgimg from '../assets/images/homePage/backgroundimage.png';
 import cdleft from '../assets/images/homePage/cdleft.svg';
-import cdright from '../assets/images/homePage/cdright.svg';
+import cdright from '../assets/images/homePage/cdright2.png';
 
 export default function HomePage() {
   return (
@@ -71,10 +71,11 @@ export default function HomePage() {
           <motion.img
             src={cd}
             style={{
-              width: '200px',
-              height: '200px',
+              width: '300px',
+              height: '300px',
               position: 'relative',
-              left: '75px',
+              left: '62px',
+              bottom: '50px',
               zIndex: '2',
             }}
             animate={{
@@ -92,28 +93,29 @@ export default function HomePage() {
               width: '150px',
               height: 'auto',
               position: 'relative',
-              top: '50px',
+              top: '-10px',
             }}
           />
           <img
             src={cdleft}
             style={{
-              width: '150px',
+              width: '190px',
               height: 'auto',
               position: 'absolute',
               zIndex: '1',
-              bottom: '50px',
+              bottom: '100px',
               left: '-40px',
             }}
           />
           <img
             src={cdright}
             style={{
-              width: '150px',
+              width: '240px',
               height: 'auto',
               position: 'absolute',
               zIndex: '1',
-              right: '30px',
+              right: '-59px',
+              bottom: '300px'
             }}
           />
           <Link to="/timetable">전체 일정 확인하기</Link>
