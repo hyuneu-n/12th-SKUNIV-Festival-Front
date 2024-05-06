4d043a785e637f20fcd79cb6d8927338cf8dccae
Hyojoong,Kim <115703110+hn7093@users.noreply.github.com>
2024-05-06 13:54:10 +0900

    repeat/모달 테스트

--- src/pages/LostPage.jsx
+++ src/pages/LostPage.jsx
@@ -4,21 +4,35 @@ import * as S from '../styles/pages/LostPage.styled';
 import ItemSwitch from '../components/ItemSwitch';
 import ItemBox from '../components/ItemBox';
 //import { LostData } from '../utils/LostData';
-import { Link , useLoaderData} from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import InputButton from '../components/InputButton';
 import i from '../assets/images/menubar/menubarImage.svg';
+import Modal from 'react-modal';
+import React, { useState } from 'react'
 export default function ItemsLost() {
-  
+  const [modalSwitch, setModalSwitch] = useState(false);
   const data = useLoaderData();
+  const checkPass = () => {
+    const inputValue = document.getElementById('pass').value;
+    if (inputValue === '7777') {
+      window.location.href = '/lostInput'; // 특정 페이지로 이동
+    } else {
+      setModalSwitch(false);
+    }
+  }
   console.log(data);
   return (
     <>
       <MenuBar></MenuBar>
+      <Modal isOpen={modalSwitch} onRequestClose={() => setModalSwitch(false)} style={ModalStyle}>
+        <h5>관리자 로그인</h5>
+        <input id='pass'></input>
+        <button onClick={() => checkPass()}>aa</button>
+      </Modal>
       <S.LostLayout>
         <S.LostHead>
-          <Link to="/lostInput">
-            <InputButton></InputButton>
-          </Link>
+          <InputButton onClick={() => setModalSwitch(true)}></InputButton>
+
         </S.LostHead>
         <S.LostImg src={i}></S.LostImg>
         <ItemSwitch />
@@ -30,7 +44,7 @@ export default function ItemsLost() {
                 name={item.lostItemName}
                 location={item.lostLocation}
                 date={item.lostDate}
-                img = {item.lostItemImagePath}
+                img={item.lostItemImagePath}
               />
             );
           })}
@@ -45,3 +59,25 @@ export async function loads() {
   console.log(response);
   return response;
 }
+const ModalStyle = {
+  overlay: {
+    position : 'fixed',
+    top : 0,
+    left : 0,
+    right : 0,
+    bottom : 0,
+    zIndex : 10,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  content : {
+    display : 'flex',
+    justifyContent : 'center',
+    alignItems : 'center',
+    flexDirection : 'column',
+    backgroundColor : 'white',
+    top : '32vh',
+    left : '12vw',
+    right : '12vw',
+    bottom : '50vh',
+  }
+};
