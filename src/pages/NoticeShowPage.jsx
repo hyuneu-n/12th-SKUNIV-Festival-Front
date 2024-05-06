967a4d0c65c3f291df2c1f0441c446d86c269c04
Kim So Yeon <qws1566@naver.com>
2024-05-06 23:12:01 +0900

    :bug: 공지사항 아이콘 추가

--- src/pages/NoticeShowPage.jsx
+++ src/pages/NoticeShowPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
-
+import edit from '../assets/images/Notice/edit.png';
+import deleted from '../assets/images/Notice/trash.png';
+import sidebar from '../assets/images/Notice/sidebar.png';
 import Footer from '../components/Footer';
 import MenuBar from '../components/MenuBar';
 import Switch from '../components/Switch';
@@ -17,6 +19,11 @@ export default function NoticeShowPage() {
     <>
     
     <MenuBar></MenuBar>
+    <S.IconCon>
+      <S.IconImg src={deleted}></S.IconImg>
+      <S.IconImg src={sidebar}></S.IconImg>
+      <S.IconImg src={edit}></S.IconImg>
+    </S.IconCon>
       <S.Title>{data.title}</S.Title>
       <S.date>{data.date}</S.date>
       <S.border></S.border>
