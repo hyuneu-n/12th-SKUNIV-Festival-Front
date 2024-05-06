aca8993c1acfaee422c46554ff9a7de940869723
Hyojoong,Kim <115703110+hn7093@users.noreply.github.com>
2024-05-06 14:00:32 +0900

    fix/모달 스타일 수정

--- src/pages/LostFindPage.jsx
+++ src/pages/LostFindPage.jsx
@@ -65,3 +65,25 @@ export async function loadLis2() {
   console.log(response);
   return response;
 }
+const ModalStyle = {
+  overlay: {
+    position : 'fixed',
+    top : 0,
+    left : 0,
+    right : 0,
+    bottom : 0,
+    zIndex : 10,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  content : {
+    display : 'flex',
+    justifyContent : 'center',
+    alignItems : 'center',
+    flexDirection : 'column',
+    backgroundColor : 'white',
+    top : '32vh',
+    left : '12vw',
+    right : '12vw',
+    bottom : '50vh',
+  }
+};
