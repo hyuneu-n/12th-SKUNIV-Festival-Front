62dd96b933ab0657690934b902cf4c57670f343b
Hyuneun <hyuneun@skuniv.ac.kr>
2024-05-09 01:11:01 +0900

    분실물

--- src/components/ItemBox.jsx
+++ src/components/ItemBox.jsx
@@ -6,9 +6,11 @@ export default function ItemBox({ name, location, date, link, img, find = false}
         <S.ItemBox>
             <S.ItemImage src={find ? complete : img} alt = "item"></S.ItemImage>
             <S.ItemLine></S.ItemLine>
-            <S.ItemName>{name}</S.ItemName>
-            <S.ItemDate>{date}</S.ItemDate>
-            <S.ItemLocation>{location}</S.ItemLocation>
+            <S.ItemContent>
+              <S.ItemName>{name}</S.ItemName>
+              <S.ItemDate>{date}</S.ItemDate>
+              <S.ItemLocation>{location}</S.ItemLocation>
+            </S.ItemContent>
         </S.ItemBox>
     );
 }
